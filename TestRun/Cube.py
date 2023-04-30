bbd16a409bdaeb54ef0cb5640d869315e9a23830
vperizes <vperizes@gmail.com>
2023-04-30 14:03:21 -0500

    Moved scripts provided by course to new folder

--- TestRun/Cube.py
+++ TestRun/Cube.py
@@ -28,6 +28,8 @@ vertices = [(0.5, -0.5, 0.5),
 triangles = [0, 2, 3, 0, 3, 1, 8, 4, 5, 8, 5, 9, 10, 6, 7, 10, 7, 11, 12,
              13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23]
 
+
+# Need to loop through triangle array 3 at a time to pick up each vertex
 def wireCube():
     for t in range(len(triangles) - 3):
         glBegin(GL_LINES)
