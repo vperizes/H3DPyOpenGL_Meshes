598a170fa33d06ef9beec2b524b5a6eef29f2400
vperizes <vperizes@gmail.com>
2023-04-30 14:05:02 -0500

    Created 2 new scripts. Mesh creates a simple mesh (one face of a cube) and includes a method for drawing the mesh. Main executes the drawing by calling the drawing method from the mesh script

---
+++ Engine/Mesh.py
@@ -0,0 +1,24 @@
+from OpenGL.GL import *
+import pygame
+
+
+class Mesh:
+    def __init__(self):
+        self.vertices = [(0.5, -0.5, 0.5),
+            (-0.5, -0.5, 0.5),
+            (0.5, 0.5, 0.5),
+            (-0.5, 0.5, 0.5),
+            (0.5, 0.5, -0.5),
+            (-0.5, 0.5, -0.5)]
+        self.triangles = [0, 2, 3, 0, 3, 1]  # this defines first two triangles on one face of a cube
+        self.draw_type = GL_LINE_LOOP
+
+
+# Method for drawing
+    def draw(self):
+        for t in range(0, len(self.triangles), 3):
+            glBegin(self.draw_type)
+            glVertex3fv(self.vertices[self.triangles[t]])
+            glVertex3fv(self.vertices[self.triangles[t + 1]])
+            glVertex3fv(self.vertices[self.triangles[t + 2]])
+            glEnd()
